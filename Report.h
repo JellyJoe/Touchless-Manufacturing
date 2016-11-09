bd0de19b92a3e05eadb3de43c70c81bc7ba0c92d
JellyJoe <joelim95@hotmail.com>
2016-11-10 02:02:10 +0800

    Seperated the report functions into a static class

---
+++ Report.h
@@ -0,0 +1,28 @@
+#ifndef __REPORT_H__
+#define __REPORT_H__
+
+#include <string>
+#include "TinyXML2.h"
+
+using namespace std;
+using namespace tinyxml2;
+
+class Report
+{
+    private:
+        Report() {}
+        static XMLDocument doc;
+        static bool IsDigitString(const string&);
+        static bool ValidateUptime(const char*);
+        static bool ValidateXMLContent();
+    public:
+        static bool ValidateDate(const char*);
+        static bool LoadXMLFile(const char*);
+        static bool SaveXMLFile(const char*);
+        static bool AddTimestamp(); // change according to data obtained from arm
+        static bool DisplaySpecificTimestamp(const string&);
+        static bool DisplayAllTimestamp();
+        static bool DeleteTimestamp(const string&);
+};
+
+#endif // __REPORT_H__
