0f484da3e646e42de2648d52ca4eb2c25223f577
SabKay <sabrinakay9988@gmail.com>
2016-12-04 18:34:34 +0800

    Add check blocks function declaration

--- pixy/pixyQT.h
+++ pixy/pixyQT.h
@@ -1,14 +1,14 @@
-
 class Pixy : public QOject
 {
     Q_OBJECT
 private:
     char incomingData[2];
+	char outgoingData[2];
     int dataLength;
 public:
     Serial * pixySP;
     explicit Pixy(QObject * parent = 0);
 public slots:
-    void getBlocks();
-
+    void getBlocks();//Execute pixy to retrieve detected blocks
+    bool checkBlocks();//To check for receive blocks is true
 };
