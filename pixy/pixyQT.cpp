15203da7d47616bfc8633f11c3206f798254cbac
SabKay <sabrinakay9988@gmail.com>
2016-12-04 16:02:53 +0800

    Modify function

--- pixy/pixyQT.cpp
+++ pixy/pixyQT.cpp
@@ -1,24 +1,31 @@
 
 Pixy::Pixy(QObject * parent) :
     QObject(parent)
+{
+    pixySP = new Serial ((char*)"COM4");
+    if(pixySP->isConnected())
     {
-        pixySP = new Serial ((char*)"COM4");
-        if(pixySP->isConnected())
-        {
-            std::cout << "Pixy is connected" << endl;
-        }
-        else
-        {
-            std::cout << "Pixy couldn't connect" << endl;
-        }
-        outgoingData[256] = "";
-        dataLength = 255;
+        std::cout << "Pixy is connected" << endl;
     }
-
-    void Pixy::getCoordinates()
+    else
     {
-        strcpy(outgoingData, "Y");
-        SP->WriteData(outgoingData, dataLength);
-	
-
+        std::cout << "Pixy couldn't connect" << endl;
     }
+    incomingData[2] = "";
+    dataLength = 2;
+}
+
+void Pixy::getBlocks()
+{
+    int result = 0;
+    result  = SP->readData(incomingData, dataLength);
+    if(result != 0)
+    {
+        cout << incomingData << endl;
+        for(int i = 0; i < sizeof(incomingData); i++)
+        {
+            incomingData[i] = '\0';
+        }
+    }
+    Sleep(500);
+}
