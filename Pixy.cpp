dc27913c32c79a8c63e1571c28fd2e93afe60030
sukhdip11 <sukhdip11@gmail.com>
2016-12-05 20:29:53 +0800

    Pixy file change and signature detection update

---
+++ Pixy.cpp
@@ -0,0 +1,80 @@
+//Header file
+#include "Arm.h"
+#include "MyClass.h"
+#include "Pixy.h"
+#include "Serial.h"
+#include <iostream>
+#include <QProcess>
+#include <QDir>
+#include <QDebug>
+
+using namespace std;
+
+Pixy::Pixy(QObject * parent) :
+    QObject(parent)
+{
+    pixySP = new Serial ((char*)"COM5");
+    if(pixySP->IsConnected())
+    {
+        qDebug() << "Pixy is connected";
+    }
+    else
+    {
+        qDebug() << "Pixy couldn't connect";
+    }
+    incomingData[MAX_DATA_LENGTH] = {'\0'};
+    outgoingData[MAX_DATA_LENGTH] = {'\0'};
+    path = "C:\\PixyMon\\bin\\PixyMon.exe";
+    check = 0;
+}
+
+//Execute pixy by entering y
+void Pixy::getBlocks()
+{
+    strcpy(outgoingData, "y");
+    pixySP->WriteData(outgoingData, MAX_DATA_LENGTH);
+
+}
+
+//Retrieve value from pixy and check if value receive is "T" means blocks detected and return true
+//else return false
+bool Pixy::checkBlocks()
+{
+    int result = 0;
+    bool validate = true;
+
+    result  = pixySP->ReadData(incomingData, MAX_DATA_LENGTH);
+    incomingData[result] = '\0';
+
+    if(incomingData[0] == 'F')
+    {
+        validate = false;
+    }
+    else if(incomingData[0] == 'T')
+    {
+        validate = true;
+    }
+
+    Sleep(500);
+
+    return validate;
+}
+
+//Execute Pixymon for signature setting
+void Pixy::executePixymon()
+{
+    qDebug() << "PixyMon run";
+    QProcess::execute(path);
+
+}
+
+//Stop the pixy
+void Pixy::stopPixy()
+{
+    //Deleting the Serial Connection
+    delete pixySP;
+    //Restarting the connection for the Arm
+    pixySP = new Serial((char*)"COM5");
+}
+
+
---
+++ Pixy.h
@@ -0,0 +1,32 @@
+#ifndef PIXY_H
+#define PIXY_H
+
+#define MAX_DATA_LENGTH 256
+#include <QObject>
+#include <QDebug>
+#include <QProcess>
+#include "Serial.h"
+
+class Pixy : public QObject
+{
+    Q_OBJECT
+
+private:
+    char incomingData[MAX_DATA_LENGTH];
+    char outgoingData[MAX_DATA_LENGTH];
+    int check;
+    QString path;
+
+public:
+    Serial * pixySP;
+    explicit Pixy(QObject * parent = 0);
+
+public slots:
+    void getBlocks();//Execute pixy
+    bool checkBlocks();//To check for receive blocks is true
+    void executePixymon();
+    void stopPixy();
+
+};
+
+#endif // PIXY_H
